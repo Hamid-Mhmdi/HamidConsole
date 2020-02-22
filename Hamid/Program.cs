165fd3a5e55e2640667f7ffd495033d8835b8e11
Hmid-Mhmdi <hamidpk0098@gmail.com>
2020-02-22 12:53:00 +0330

    chenged write

--- Hamid/Program.cs
+++ Hamid/Program.cs
@@ -6,7 +6,7 @@ namespace Hamid
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("salam");
         }
     }
 }
